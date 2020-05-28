a3353be6800821caba9c2acb2d127a4330d1a847
TOUKAM ZANJIO HAROLD WILSON <harold.toukam-zanjio.1@ulaval.ca>
2020-05-28 09:57:37 -0400

    __init__ de piece ok!!!

--- piece.py
+++ piece.py
@@ -1,7 +1,7 @@
 class ChessError(Exception):
     """
     Cette classe retourne des erreurs
-    lorsque la classe Quoridor a des problèmes
+    lorsque le jeu Chess a des problèmes
     """
 
 
@@ -11,15 +11,32 @@ class Piece():
 
     def __init__(self, nom, couleur):
         """Creating a piece object, with its attributes :
-    - 'nom' as name (ROI, DAME...);
-    - 'couleur' as color (blanc,noir);
-    - 'valeur' as its value"""
+        - 'nom' as name (ROI, DAME...);
+        - 'couleur' as color (blanc,noir);
+        - 'valeur' as its value
+        """
+        #initialisation des elements importants
+        VIDE = '.'
+        nomPiece=[VIDE,'ROI','DAME','TOUR','CAVALIER','FOU','PION']
+        valeurPiece=[0,0,9,5,3,3,1]
+        
+        #verification et erreurs de  création 
+        if not couleur in ["blanc", "noir"]:
+            raise ChessError("La couleur entrée n'est ni du blanc ni du noir")
+        if not nom in nomPiece:
+            raise ChessError('Le nom de votre piece ne correspond à aucun nom prédéfini')
+        
+        #creation des instances de la classe
+        self.nom = nom
+        self.couleur = couleur
+        self.valeur = valeurPiece[nomPiece.index(self.nom)]
         pass
 
     def isEmpty(self):
         """Returns TRUE or FALSE if this piece object is defined,
             As any square on board can have a piece on it, or not,
-            we can set a null piece on a square."""
+            we can set a null piece on a square.
+        """
         pass
 
     def pos2_roi(self):
@@ -28,23 +45,27 @@ class Piece():
     def pos2_tour(self, pos1, cAd, echiquier):
         """Returns the list of moves for a ROOK :
         - at square number 'pos1' (0 to 63)
-        - opponent color is cAd (blanc,noir)"""
+        - opponent color is cAd (blanc,noir)
+        """
         pass
 
     def pos2_cavalier(self, pos1, cAd, echiquier):
         """Returns the list of moves for a KNIGHT :
         - at square number 'pos1' (0 to 63)
-        - opponent color is cAd (blanc,noir)"""
+        - opponent color is cAd (blanc,noir)
+        """
         pass
 
     def pos2_fou(self, pos1, cAd, echiquier):
         """Returns the list of moves for a BISHOP :
         - at square number 'pos1' (0 to 63)
-        - opponent color is cAd (blanc,noir)"""
+        - opponent color is cAd (blanc,noir)
+        """
         pass
 
     def pos2_pion(self, pos1, couleur, echiquier):
         """Returns the list of moves for a PAWN :
         - at square number 'pos1' (0 to 63)
-        - opponent color is cAd (blanc,noir)"""
+        - opponent color is cAd (blanc,noir)
+        """
         pass
